2085cd567ec6d6b19273ff2ccff4cc13cb1d06a1
imdadhussain <imdadhussain530@gmail.com>
2021-12-04 13:01:08 +0530

    Fix the issue

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
         user.set_password(password)
-        user.save(using=self.__db)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, name, password):
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self.__db)
+        user.save(using=self._db)
         return user
 
 
